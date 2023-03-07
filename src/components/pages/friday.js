876fc36216f6e813492a284e68e7cf7962f4f64a
Cristina Baiu <115459671+FineLine01@users.noreply.github.com>
2023-03-07 12:15:43 +0000

    pages

---
+++ src/components/pages/friday.js
@@ -0,0 +1,11 @@
+function Friday(){
+    return (
+        <>
+            <h1>Today is Friday</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem dolore veritatis maxime dignissimos quis! Deleniti tenetur numquam explicabo ducimus! Laborum perferendis corporis deleniti fuga amet repellendus quibusdam aut asperiores impedit?
+            </p>
+        </>
+    )
+}
+export default Friday;
---
+++ src/components/pages/tuesday.js
@@ -0,0 +1,11 @@
+function Tuesday(){
+    return (
+        <>
+            <h1>Today is Tuesday</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem dolore veritatis maxime dignissimos quis! Deleniti tenetur numquam explicabo ducimus! Laborum perferendis corporis deleniti fuga amet repellendus quibusdam aut asperiores impedit?
+            </p>
+        </>
+    )
+}
+export default Tuesday;
